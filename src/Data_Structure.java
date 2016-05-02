dffc57a0e976bb23f5596ba68c7bd329f8a60b2d
robertwei81 <robertwei@yahoo.com>
2016-05-02 00:06:37 -0700

    Data/&/Test : incomplete
    ... working on...

--- src/Data_Structure.java
+++ src/Data_Structure.java
@@ -1,5 +1,22 @@
 
 public class Data_Structure {
-	
-
+	Node[] Nodes;
+	float [] Tokens;
+	Data_Structure(){
+		 Nodes = new Node[150];
+		 Tokens = new float [150];		// all start at zero
+	}
+	void UpdateNode(){
+		//TODO check token to match with current thread
+		//TODO set token
+		//TODO update node
+		//TODO trigger update on other system, wait till done
+		//TODO reset token value
+	}
 }
+/*
+DATA STRUCTURE
+The assignment is to maintain a fixed size set of 150 nodes, which are replicated (duplicated) on 3 computers.
+The distributed set is to be coherent at real time, i.e., each worker thread operates on an up-to-date node. 
+That is, the software must continuously maintain the set & its nodes across the network.
+*/
