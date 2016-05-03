b0bd24255037b97763db3b6fee8f4d8de7130ff6
Christian Whittington <christiangwhittington@gmail.com>
2016-05-03 16:40:19 -0700

    acceptor/requestor
    send and receive objects now, Node or Token

--- src/Acceptor.java
+++ src/Acceptor.java
@@ -1,31 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
-public class Acceptor {
-	public Acceptor(){
-	}
-	public static void readMessage(int portNumber, String host){
+public class Acceptor{
+	public Acceptor(){}
+	
+	public static void readMessage(int portNumber, String host, int x){
 		try{
 			InetAddress hostName = InetAddress.getByName(host);
-			int acceptorPort = portNumber;
-			Socket mySocket = new Socket(hostName, acceptorPort);
-			System.out.println("Connection request granted");
-			InputStream inStream = mySocket.getInputStream();
-			BufferedReader socketInput = new BufferedReader(new InputStreamReader(inStream));
-			System.out.println("waiting to read");
-			String message = socketInput.readLine();
-			System.out.println("Message received:");
-			System.out.println("\t" + message);
+			Socket mySocket = new Socket(hostName, portNumber);
+			ObjectInputStream dataIn = new ObjectInputStream(mySocket.getInputStream());
+			if(x == 0){
+				Node inObject = (Node) dataIn.readObject();
+			}else{
+				Token inObject = (Token) dataIn.readObject();
+				System.out.println(inObject.mIndex);
+			}
 			mySocket.close();
-			System.out.println("data socket closed");
 		}catch (Exception e){
 			
 		}
 	}
 	public static void main(String[] args){
-		Acceptor.readMessage(1234, "192.168.0.8");
+		//Acceptor.readMessage(1234, "192.168.0.8");
 	}
 }
