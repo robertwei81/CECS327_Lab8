9db1b7900a632630f659b6cf08314ec4b201f445
Christian Whittington <christiangwhittington@gmail.com>
2016-05-04 12:04:43 -0700

    had names wrong

--- src/Acceptor.java
+++ src/Acceptor.java
@@ -3,34 +3,51 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Acceptor {
-	Socket mySocket;
-	ObjectInputStream dataIn;
-	InetAddress hostName;
-	public Acceptor(int portNumber, String host){
+public class Acceptor extends Thread{
+	Socket dataSocket;
+	ObjectOutputStream dataOut;
+	ServerSocket connectionSocket;
+	public Acceptor(){}
+	public Acceptor(int portNum){
+		try {
+			this.connectionSocket = new ServerSocket(portNum);
+			this.dataSocket = connectionSocket.accept();
+			this.dataOut = new ObjectOutputStream(dataSocket.getOutputStream());
+		} catch (IOException e) {}
+	}
+	public Acceptor(int portNum,Token footballUpdate){
+		try {
+			connectionSocket = new ServerSocket(portNum);
+			dataSocket = connectionSocket.accept();
+			dataOut = new ObjectOutputStream(dataSocket.getOutputStream());
+		} catch (IOException e) {}
+		//footballUpdate.mSystemIP;	//from which host
+		//footballUpdate.mThreadID;	//on that host which thread
+		//footballUpdate.mIndex;	//updating which node
+		//footballUpdate.mTokenState;	//what are we doing? 
+		//0-request token, 1-permission received okay, 2-request update
+	}
+	public void sendToken(Token outToken){
+		try {
+			this.dataOut.writeObject(outToken);
+			this.dataOut.flush();
+		} catch (IOException e) {}
+	}
+	public void sendNode(Node outNode){
+		try {
+			this.dataOut.writeObject(outNode);
+			this.dataOut.flush();
+		} catch (IOException e) {}
+	}
+	public void closeSocket(){
 		try{
-			hostName = InetAddress.getByName(host);
-			mySocket = new Socket(hostName, portNumber);
-			dataIn = new ObjectInputStream(mySocket.getInputStream());
-		}catch(Exception e){
-			
-		}
-	}
-	public Node readNode() throws ClassNotFoundException, IOException{	
-		Node inObject = (Node) this.dataIn.readObject();
-		return inObject;
-	}
-	public Token readToken() throws ClassNotFoundException, IOException{	
-		Token inObject = (Token) this.dataIn.readObject();
-		return inObject;
-	}
-	public void closeSocket() throws IOException{
-		this.mySocket.close();
-	}
-	public static void main(String[] args){
-		//Acceptor.readMessage(1234, "192.168.0.8");
+			this.dataSocket.close();
+			this.connectionSocket.close();
+		} catch(IOException e){}
 	}
 }
--- src/Requestor.java
+++ src/Requestor.java
@@ -1,55 +1,37 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 
-public class Requestor{
-	Socket dataSocket;
-	ObjectOutputStream dataOut;
-	ServerSocket connectionSocket;
-	public Requestor(){}
-	public Requestor(int portNum){
-		try {
-			this.connectionSocket = new ServerSocket(portNum);
-			this.dataSocket = connectionSocket.accept();
-			this.dataOut = new ObjectOutputStream(dataSocket.getOutputStream());
-		} catch (IOException e) {}
-	}
-	public Requestor(int portNum,Token footballUpdate){
-		try {
-			connectionSocket = new ServerSocket(portNum);
-			dataSocket = connectionSocket.accept();
-			dataOut = new ObjectOutputStream(dataSocket.getOutputStream());
-		} catch (IOException e) {}
-		//footballUpdate.mSystemIP;	//from which host
-		//footballUpdate.mThreadID;	//on that host which thread
-		//footballUpdate.mIndex;	//updating which node
-		//footballUpdate.mTokenState;	//what are we doing? 
-		//0-request token, 1-permission received okay, 2-request update
-	}
-	public void sendToken(Token outToken){
-		try {
-			this.dataOut.writeObject(outToken);
-			this.dataOut.flush();
-		} catch (IOException e) {}
-	}
-	public void sendNode(Node outNode){
-		try {
-			this.dataOut.writeObject(outNode);
-			this.dataOut.flush();
-		} catch (IOException e) {}
-	}
-	public void closeSocket(){
+public class Requestor extends Thread{
+	Socket mySocket;
+	ObjectInputStream dataIn;
+	InetAddress hostName;
+	public Requestor(int portNumber, String host){
 		try{
-			this.dataSocket.close();
-			this.connectionSocket.close();
-		} catch(IOException e){}
+			hostName = InetAddress.getByName(host);
+			mySocket = new Socket(hostName, portNumber);
+			dataIn = new ObjectInputStream(mySocket.getInputStream());
+		}catch(Exception e){
+			
+		}
 	}
-	public static void main(String[] args){
-		//Requestor.sendData(5000, new Token());
+	public Node readNode() throws ClassNotFoundException, IOException{	
+		Node inObject = (Node) this.dataIn.readObject();
+		return inObject;
 	}
+	public Token readToken() throws ClassNotFoundException, IOException{	
+		Token inObject = (Token) this.dataIn.readObject();
+		return inObject;
+	}
+	public void closeSocket() throws IOException{
+		this.mySocket.close();
+	}
+	
 }
